93a117dbf6ac4ae5df4dd7e1142449850b7717b5
user <230528@astanait.edu.kz>
2024-12-16 20:45:55 +0600

    attributtes changes in movie and review

--- models/movie.go
+++ models/movie.go
@@ -4,6 +4,10 @@ package models
 type Movie struct {
 	ID          uint   `json:"id" gorm:"primaryKey"`
 	Title       string `json:"title"`
-	Description string `json:"description"`
+	Director string `json:"director"`
+	Country string `json:"country"`
+	Genres 	[]string `json:"genres"`
 	ReleaseYear int    `json:"release_year"`
+	Description string `json:"description"`
+
 }
--- models/review.go
+++ models/review.go
@@ -3,6 +3,7 @@ package models
 
 type Review struct {
 	ID      uint   `json:"id" gorm:"primaryKey"`
+	Rating float32 `json:"rating"`
 	Content string `json:"content"`
 	UserID  uint   `json:"user_id"`
 	MovieID uint   `json:"movie_id"`
