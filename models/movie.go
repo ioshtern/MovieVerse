82353dfbcdfc2ad3ee872cca1491677fa219772a
alane <alan.ersainov2004@gmail.com>
2024-12-16 23:56:53 +0500

    updated: changed Genres datatype from text to JSON

--- models/movie.go
+++ models/movie.go
@@ -1,13 +1,13 @@
 package models
 
+import "gorm.io/datatypes"
 
 type Movie struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	Title       string `json:"title"`
-	Director string `json:"director"`
-	Country string `json:"country"`
-	Genres 	[]string `json:"genres"`
-	ReleaseYear int    `json:"release_year"`
-	Description string `json:"description"`
-
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	Title       string         `json:"title"`
+	Director    string         `json:"director"`
+	Country     string         `json:"country"`
+	Genres      datatypes.JSON `json:"genres"`
+	ReleaseYear int            `json:"release_year"`
+	Description string         `json:"description"`
 }
