c7c14dbf8d360875b9e6d54cd2a7284e8738f756
alane <alan.ersainov2004@gmail.com>
2025-01-23 18:41:14 +0500

    add: deployment, gitignore, env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"MovieVerse/controllers"
 	"MovieVerse/models"
 	"encoding/json"
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/time/rate"
 	"gorm.io/driver/postgres"
@@ -71,19 +72,28 @@ func logAction(fields logrus.Fields, message string) {
 }
 
 func initDatabase() {
-	var err error
-	dsn := "user=postgres password=3052 dbname=movieverse port=5433 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := "user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=" + os.Getenv("DB_SSLMODE")
+
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		logger.WithError(err).Fatal("Failed to connect to the database")
+		log.Fatal("Failed to connect to the database:", err)
 	}
-	logger.Info("Database connected successfully")
+	log.Println("Database connected successfully")
 
 	err = db.AutoMigrate(&models.User{}, &models.Movie{}, &models.Review{})
 	if err != nil {
-		logger.WithError(err).Fatal("Database migration failed")
+		log.Fatal("Database migration failed:", err)
 	}
-	logger.Info("Database migrated successfully")
+	log.Println("Database migrated successfully")
 }
 
 func handlePostRequest(w http.ResponseWriter, r *http.Request) {
